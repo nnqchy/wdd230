5df8f219db59cc45743567247202a20d53956028
nnqchy <backstagepc@gmail.com>
2024-02-16 09:58:56 -0800

    021624_0958

--- lesson05/scripts/escritura.js
+++ lesson05/scripts/escritura.js
@@ -1,18 +1,21 @@
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
-const list = document.querySelector('ul');
+const list = document.querySelector('#list');
 
 button.addEventListener('click', function () {
     if (input.value != '') {
 
+        let li = document.createElement('li');
+        let deleteButton = document.createElement('button');
+        li.textContent = input.value;
+        deleteButton.textContent = '❌';
+        li.append(deleteButton);
+        deleteButton.addEventListener('click', function () {
+            list.removeChild(li);
+            input.focus();
+        })
+        list.append(li);
     }
-    const li = document.createElement('li');
-    const deleteButton = document.createElement('button');
-    li.textContent = input.value;
-    deleteButton.textContent = '❌';
-    li.append(deleteButton);
-    list.append(li);
-
 
 });
 
