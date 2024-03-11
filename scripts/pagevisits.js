c8ae7a4be09c6b534f01a2de57c5fb39fea8f6ee
nnqchy <backstagepc@gmail.com>
2024-03-11 08:34:40 -0700

    240311_0833

--- scripts/pagevisits.js
+++ scripts/pagevisits.js
@@ -8,7 +8,7 @@ let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
 if (numVisits !== 0) {
     visitsDisplay.textContent = numVisits;
 } else {
-    visitsDisplay.textContent = `This is your first visit. 🥳 Welcome!`;
+    visitsDisplay.textContent = `This is your first visit. 🥳 Welcome-Bienvenido!`;
 }
 
 // 4. increment the number of visits by one.
