df84fd3841d2ad46690d58559ac5c4f5216d5a7d
nnqchy <backstagepc@gmail.com>
2024-03-21 23:50:21 -0700

    240321_2350

--- scripts/links.js
+++ scripts/links.js
@@ -6,6 +6,7 @@ async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
     console.log(data);
+    displayLinks(data.links);
 }
 
 getLinks();
