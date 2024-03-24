45019db25563b9c97e6ec6a68e92580745c7a64b
nnqchy <backstagepc@gmail.com>
2024-03-23 18:48:51 -0700

    1848

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,65 +1,63 @@
-// const baseURL = "https://nnqchy.github.io/wdd230/";
-// const companiesData = "https://nnqchy.github.io/wdd230/data/links.json";
 const baseURL = "https://nnqchy.github.io/wdd230/chamber";
 const companiesData = "https://nnqchy.github.io/wdd230/chamber/data/members.json";
 //button declarations
-// const gridbutton = document.querySelector("#grid");
-// const listbutton = document.querySelector("#list");
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
 // //display members declaration
-// const display = document.querySelector("article");
+const display = document.querySelector("article");
 
 // The following code could be written cleaner. How? We may have to simplfiy our HTMl and think about a default view.
 
-// gridbutton.addEventListener("click", () => {
-//     // example using arrow function
-//     display.classList.add("grid");
-//     display.classList.remove("list");
-// });
+gridbutton.addEventListener("click", () => {
+    // example using arrow function
+    display.classList.add("grid");
+    display.classList.remove("list");
+});
 
-// listbutton.addEventListener("click", showList); // example using defined function
+listbutton.addEventListener("click", showList); // example using defined function
 
-// function showList() {
-//     display.classList.add("list");
-//     display.classList.remove("grid");
-// }
+function showList() {
+    display.classList.add("list");
+    display.classList.remove("grid");
+}
 
 
 
 // from Get Companies //
 
-// const business = document.querySelector('#bussines');
+const business = document.querySelector('#bussines');
 
 
 async function getCompanies() {
     const response = await fetch(companiesData);
     const data = await response.json();
     console.log(data);
-    //displayLinks(data.members);
+    displayLinks(data.members);
 
 
 }
 
-// const displayLinks = (companies) => {
-//     companies.forEach((company) => {
-//         // Create elements to add to the div.cards element
-//         let card = document.createElement('section');
-//         let ul = document.createElement('ul');
-//         let li = document.createElement('li');
-//         let a = document.createElement('a');
+const displayLinks = (companies) => {
+    companies.forEach((company) => {
+        // Create elements to add to the div.cards element
+        let card = document.createElement('section');
+        let ul = document.createElement('ul');
+        let li = document.createElement('li');
+        let a = document.createElement('a');
 
-//         a.textContent = company.name;
-//         a.setAttribute('href', company.url);
-//         // Build the list
-//         li.textContent = `Week ${company.address}`;
+        a.textContent = company.name;
+        a.setAttribute('href', company.url);
+        // Build the list
+        li.textContent = `Week ${company.address}`;
 
 
-//         // Append the section(card) with the created elements
-//         card.appendChild(ul);
-//         ul.appendChild(li);
-//         ul.appendChild(a);
+        // Append the section(card) with the created elements
+        card.appendChild(ul);
+        ul.appendChild(li);
+        ul.appendChild(a);
 
-//         business.appendChild(card);
-//     });
-// }
+        business.appendChild(card);
+    });
+}
 
 getCompanies();
