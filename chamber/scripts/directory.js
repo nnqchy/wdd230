9913d1ed0d37bc85674c868bd1ef1b6334aba38b
nnqchy <backstagepc@gmail.com>
2024-03-23 17:51:34 -0700

    1723

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,25 +1,63 @@
-const linksURL = "https://nnqchy.github.io/wdd230/chamber/data/members.json";
-const layout = document.querySelector(".layout");
+const baseURL = "https://nnqchy.github.io/wdd230/";
+const linksURL = "https://nnqchy.github.io/wdd230/data/links.json";
+//button declarations
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+//display members declaration
+const display = document.querySelector("article");
 
+// The following code could be written cleaner. How? We may have to simplfiy our HTMl and think about a default view.
 
-document.addEventListener("DOMContentLoaded", () => {
-    layout.classList.add("grid");
+gridbutton.addEventListener("click", () => {
+    // example using arrow function
+    display.classList.add("grid");
+    display.classList.remove("list");
 });
 
+listbutton.addEventListener("click", showList); // example using defined function
 
-
-
-
-async function getMembers(url) {
-    const response = await fetch(url);
-
-    if (response.ok) {
-        const data = await response.json();
-        console.table(data);
-
-        displayMembers(data.name);
-    }
+function showList() {
+    display.classList.add("list");
+    display.classList.remove("grid");
 }
 
 
-getMembers(linksURL);
+
+// from Get Links //
+
+const business = document.querySelector('#bussines');
+
+
+async function getMembers() {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    console.log(data.members);
+    displayLinks(data.members);
+
+
+}
+
+const displayLinks = (members) => {
+    members.forEach((member) => {
+        // Create elements to add to the div.cards element
+        let card = document.createElement('section');
+        let ul = document.createElement('ul');
+        let li = document.createElement('li');
+        let a = document.createElement('a');
+
+        a.textContent = member.name;
+        a.setAttribute('href', member.url);
+        // Build the list
+        li.textContent = `Week ${membern.address}`;
+
+
+        // Append the section(card) with the created elements
+        card.appendChild(ul);
+        ul.appendChild(li);
+        ul.appendChild(a);
+
+        business.appendChild(card);
+    });
+}
+
+getLinks();
