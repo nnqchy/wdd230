6ffa8e94ad6add20bfc2b81ab0c27d90337c6325
nnqchy <backstagepc@gmail.com>
2024-03-27 20:56:45 -0700

    240327_2056

---
+++ chamber/lesson10/scripts/weather.js
@@ -0,0 +1,23 @@
+//select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=32b3c0930f8c09b95ee90430ea66f307';
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data); // testing only
+            // displayResults(data); // uncomment when ready
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+apiFetch();
